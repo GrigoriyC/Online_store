2bd39c05c8a7c12e29c9953e640b5d0a4bee626c
GrigoriyS <smolencevgv@mail.ru>
2025-12-05 18:01:42 +0700

    refactor(products): Оптимизировал вьюшку создания товара (forms.Form)

--- store/views.py
+++ store/views.py
@@ -18,6 +18,7 @@ def get_product_detail(request, product_id):
 
 
 def create_product(request):
+  form = ProductForm(request.POST or None)
     
   if request.method == "POST":
     form = ProductForm(request.POST)    
@@ -29,10 +30,6 @@ def create_product(request):
       )
     
       return redirect('product_detail', product_id=product.id)
-    else:
-      return render(request, 'store/product_add.html', {"form": form})     
-    
-  # GET
-  form = ProductForm()
+    # Если форма невалидна, продолжим к render ниже.
 
   return render(request, 'store/product_add.html', {"form": form})
